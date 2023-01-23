97da6ab9afbe68efc3e9359122aacf17e287e9fd
Sephirah <nicolas.bussiere@outlook.fr>
2023-01-23 14:11:38 +0100

    Add class to element in ProfileSelection

--- src/screens/ProfileSelection.js
+++ src/screens/ProfileSelection.js
@@ -2,21 +2,21 @@ import React from 'react'
 
 const ProfileSelection = () => {
     return (
-        <div>
-            <div>
+        <div className="profileListContainer">
+            <div className="profile">
                 <img src="../img/ghost_netflux.png" />
                 <p>Random 1</p>
             </div>
-            <div>
-                <img/>
+            <div class="profile">
+                <img src="../img/ghost_netflux.png"/>
                 <p>Random 2</p>
             </div>
-            <div>
-                <img />
+            <div class="profile">
+                <img src="../img/ghost_netflux.png"/>
                 <p>Random 3</p>
             </div>
-            <div>
-                <img/>
+            <div class="profile">
+                <img src="../img/ghost_netflux.png"/>
                 <p>Random 4</p>
             </div>
         </div>
