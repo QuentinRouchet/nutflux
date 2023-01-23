86aa9199aaf704e149fc079bd37f49c46313c4b6
Sephirah <nicolas.bussiere@outlook.fr>
2023-01-23 12:00:17 +0100

    Add profile in profile selection page (hardcode)

---
+++ src/screens/ProfileSelection.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const ProfileSelection = () => {
+    return (
+        <div>
+            <div>
+                <img/>
+                <p>Random 1</p>
+            </div>
+            <div>
+                <img/>
+                <p>Random 2</p>
+            </div>
+            <div>
+                <img />
+                <p>Random 3</p>
+            </div>
+            <div>
+                <img/>
+                <p>Random 4</p>
+            </div>
+        </div>
+    )
+}
+
+export default ProfileSelection;
